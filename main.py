c61c069d9dffb0c7547ec13962c5e60a142f2153
Anaghjaiswar <jaiswaranagh@gmail.com>
2024-10-20 11:21:33 +0530

    player animation done + pictures moved out of folder

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
-import pygame
-from sys import exit
+import pygame, sys
+from player import Player
 
 class Game:
     def __init__(self):
-        pass
+        player_sprite = Player((screen_width / 2,screen_height),screen_width,5)
+        self.player = pygame.sprite.GroupSingle(player_sprite)
 
-    def __init__(self):
-        pass
-
-
+    def run(self):
+        self.player.update()
+        self.player.draw(screen)
 
 
 if __name__ == '__main__':
-    pygame.init
+    pygame.init()
     # screen setup
     screen_width = 600
     screen_height = 600
     screen = pygame.display.set_mode((screen_width,screen_height))
-
     clock = pygame.time.Clock()
     game = Game()
 
     # gmae runs until condition is true
-    while True():
+    while True:
 
         # quit button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                exit()
+                sys.exit()
 
         screen.fill((30,30,30))
         game.run()
